a67c0a6a231cc4ffcf10e35bd2703724e387ece1
yshak <Vyshakn29@gmail.com>
2025-03-27 01:39:58 +0530

    menubar added

--- components/textEditor/Dmeditor.tsx
+++ components/textEditor/Dmeditor.tsx
@@ -2,19 +2,23 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Menubar from './Menubar'
 
 const DmEditor = () => {
-  const editor = useEditor({
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
-    editorProps:{
-        attributes:{
-            class:"bg-UIslate-50 border-2 border-UIslate-300 rounded-xl w-2xl h-72 p-4"
+    const editor = useEditor({
+        extensions: [StarterKit],
+        content: '<p>Hello World! 🌎️</p>',
+        editorProps: {
+            attributes: {
+                class: "bg-UIslate-50 border-2 border-UIslate-300 rounded-xl w-2xl h-72 p-4"
+            }
         }
-    }
-  })
+    })
 
-  return <EditorContent editor={editor} />
+    return (<>
+    <Menubar editor={editor} />
+<EditorContent editor={editor} />
+    </>)
 }
 
 export default DmEditor
